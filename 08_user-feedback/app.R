9f8d81c4818467f1036fef7ca99f1f759788a4f2
taylordunn <t.dunn19@gmail.com>
2021-12-04 17:14:55 -0400

    Chapter 8

--- 08_user-feedback/app.R
+++ 08_user-feedback/app.R
@@ -124,3 +124,112 @@ server6 <- function(input, output, session) {
 }
 
 shinyApp(ui6, server6)
+
+ui7 <- fluidPage(
+  waiter::use_waitress(),
+  numericInput("steps", "How many steps?", 10),
+  actionButton("go", "go"),
+  textOutput("result")
+)
+
+server7 <- function(input, output, session) {
+  data <- eventReactive(input$go, {
+    waitress <- waiter::Waitress$new(max = input$steps,
+                                     theme = "overlay-percent")
+    on.exit(waitress$close())
+
+    for (i in seq_len(input$steps)) {
+      Sys.sleep(0.5)
+      waitress$inc(1)
+    }
+
+    runif(1)
+  })
+
+  output$result <- renderText(round(data(), 2))
+}
+
+shinyApp(ui7, server7)
+
+ui8 <- fluidPage(
+  waiter::use_waiter(),
+  actionButton("go", "go"),
+  textOutput("result")
+)
+
+server8 <- function(input, output, session) {
+  data <- eventReactive(input$go, {
+    waiter <- waiter::Waiter$new()
+    waiter$show()
+    on.exit(waiter$hide())
+
+    Sys.sleep(sample(5, 1))
+    runif(1)
+  })
+  output$result <- renderText(round(data(), 2))
+}
+
+shinyApp(ui8, server8)
+
+ui9 <- fluidPage(
+  waiter::use_waiter(),
+  actionButton("go", "go"),
+  plotOutput("plot"),
+)
+
+server9 <- function(input, output, session) {
+  data <- eventReactive(input$go, {
+    waiter::Waiter$new(id = "plot")$show()
+
+    Sys.sleep(3)
+    data.frame(x = runif(50), y = runif(50))
+  })
+
+  output$plot <- renderPlot(plot(data()), res = 96)
+}
+
+shinyApp(ui9, server9)
+
+ui10 <- fluidPage(
+  actionButton("go", "go"),
+  shinycssloaders::withSpinner(plotOutput("plot")),
+)
+server10 <- function(input, output, session) {
+  data <- eventReactive(input$go, {
+    Sys.sleep(3)
+    data.frame(x = runif(50), y = runif(50))
+  })
+
+  output$plot <- renderPlot(plot(data()), res = 96)
+}
+
+shinyApp(ui10, server10)
+
+modal_confirm <- modalDialog(
+  "Are you sure you want to continue?",
+  title = "Deleting files",
+  footer = tagList(
+    actionButton("cancel", "Cancel"),
+    actionButton("ok", "Delete", class = "btn btn-danger")
+  )
+)
+
+ui11 <- fluidPage(
+  actionButton("delete", "Delete all files?")
+)
+
+server11 <- function(input, output, session) {
+  observeEvent(input$delete, {
+    showModal(modal_confirm)
+  })
+
+  observeEvent(input$ok, {
+    showNotification("Files deleted")
+    removeModal()
+  })
+  observeEvent(input$cancel, {
+    removeModal()
+  })
+}
+
+shinyApp(ui11, server11)
